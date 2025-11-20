9be6b90694f820216ce660f78dfbab453886fc5d
SimonAndersson0 <151626143+SimonAndersson0@users.noreply.github.com>
2025-11-20 19:27:03 +0100

    Endpoint template done

---
+++ api-helper-backend copy/.dev/templates/ENDPOINTS-template.php
@@ -0,0 +1,28 @@
+<?php
+require_once __DIR__ . '/../../classes/GroupHandler.php';
+require_once __DIR__ . '/../../helpers/Response.php';
+
+$handler = new GroupHandler();
+
+//check required parameters         MARK:parameters
+$reqparameter=['title','project_id'];
+foreach($reqparameter as $param){
+    if(!isset($data[$param])){
+        Response::error("Missing parameter: ".$param);
+    }
+}
+
+
+//set all parameters 
+
+//required parameters
+$title = $data['title'];
+$project_id = $data['project_id'];
+//optional parameters
+$parent_id=$data['parent_id'] ?? NULL; //default to NULL  if not provided only needed for non required parameters
+
+
+//method call
+echo $handler->createGroup($project_id, $name, $parent_id);
+
+?>
